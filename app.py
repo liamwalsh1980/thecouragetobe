8384e89606ebad3cd30a7c428cba927d672244bb
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-04-06 20:32:58 +0000

    Added views and started nav bar header

--- app.py
+++ app.py
@@ -1,15 +1,53 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+# Create a variable and an instance of Flask
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# Configuration to grab the database name
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# Configure the actual connection string which is the Mongo URI
+app.secret_key = os.environ.get("SECRET_KEY")
+# Graps the secret_key
+
+mongo = PyMongo(app)
+# An instance of PyMongo using a Constructor method to add 'app' from above
+
+
+# @app.route("/")
+# @app.route("/get_blogs")
+# def get_blogs():
+#     blogs = mongo.db.blogs.find()
+#     return render_template("blogs.html", blogs=blogs)
 
 
 @app.route("/")
-def hello():
-    return "Welcome to thecouragetobe....."
+@app.route("/index")
+def index():
+    return render_template("index.html", index=index)
+
+
+@app.route("/proctors_model")
+def proctors_model():
+    return render_template("proctors_model.html")
+
+
+@app.route("/protecting_you")
+def protecting_you():
+    return render_template("protecting_you.html")
+
+
+@app.route("/blog")
+def blog():
+    blog = mongo.db.blog.find()
+    return render_template("blog.html", blog=blog)
 
 
 if __name__ == "__main__":
