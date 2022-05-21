7de4df33d498559c500014eeede353f309ac840e
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-05-21 21:20:13 +0000

    Categories page completed and buttons updated across templates

--- app.py
+++ app.py
@@ -185,6 +185,12 @@ def delete_post(post_id):
     return redirect(url_for("blog_admin", username=session["user"]))
 
 
+@app.route("/categories")
+def categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
