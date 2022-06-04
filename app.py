42f691d3db16b2761df7d9288ce01ab379cd8a13
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2022-06-04 08:38:37 +0000

    debug updated

--- app.py
+++ app.py
@@ -229,4 +229,4 @@ def delete_category(category_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
