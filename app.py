a92a7d65f6f35f7257c08dc89d743e63eb2f2833
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2024-01-17 21:19:14 +0000

    debug changed to true

--- app.py
+++ app.py
@@ -245,4 +245,4 @@ def delete_category(category_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
